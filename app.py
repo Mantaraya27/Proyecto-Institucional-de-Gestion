bfa8709f01bb80fef4fdba9bbcf9e5114141e922
Mantaraya27 <lucassosavega@gmail.com>
2025-08-05 20:52:59 -0300

    Initial commit: Sistema de Gestión de Reportes Institucionales

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def crear_app():
     app.config['MAIL_USE_SSL'] = False
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_PASSWORD'] = '1234'
     app.config['MYSQL_DB'] = 'informatica'
     app.secret_key = 'mysecretkey'
     mail = Mail(app)
